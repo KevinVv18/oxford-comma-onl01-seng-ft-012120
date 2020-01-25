253b4eb0a58a41d56eae3f36001ccfa63151bb85
Kevin Villalobos <kevinv15v@gmail.com>
2020-01-25 01:41:17 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,13 +4,8 @@ def oxford_comma(array)
   array.join
   elsif array.length == 1
   	array.join
-<<<<<<< HEAD
-  else array.length > 2
-    array[-1].prepend "and "
-=======
   elsif array.length > 2
-    array[-1].push "and "
->>>>>>> f62401c10ba83bbac0fef0e6adcf9c01189474d9
+    array[-1].prepend "and "
 	array.join(", ")
   end
 end
